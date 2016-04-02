40851815aecfcb0b6f46edaa68f232e5664143a4
unknown <thuskinson@thuskinson.OptimalBlue.local>
2016-04-01 21:54:24 -0500

    adding a couple challenges

---
+++ CodeEval_CSharp_ReverseAndAdd/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ReverseAndAdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText(args[0]))
+                
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    int iterations = 0;
+                    revAndAdd(line);
+                    while (!isPalindrome(line))
+                    {
+                        line = revAndAdd(line);
+                        iterations++;
+                    }
+
+                    Console.Out.WriteLine(iterations + " " + line);
+                }
+
+        }
+
+        static string revAndAdd(string value)
+        {
+            StringBuilder rev = new StringBuilder(value);
+            char temp;
+            for (int i = 0; i < value.Length / 2; i++)
+            {
+                temp = rev[rev.Length-1-i];
+                rev[rev.Length-1-i] = rev[i];
+                rev[i] = temp;
+            }
+            return addStrings(value, rev.ToString());
+        }
+
+        static string addStrings(string a, string b)
+        {
+            StringBuilder ret = new StringBuilder();
+            int carry = 0;
+            for (int i = a.Length - 1; i >= 0; i--)
+            {
+                int sum = (a[i] + b[i]) - (48 * 2) + carry;
+                carry = sum / 10;
+                ret.Insert(0, (char)((sum % 10)+48));
+            }
+            if (carry > 0)
+            {
+                ret.Insert(0, (char)(carry+48));
+            }
+
+            return ret.ToString();
+        }
+
+        static bool isPalindrome(string value)
+        {
+            for (int i = 0; i < value.Length / 2; i++)
+            {
+                if (value[i] != value[value.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        
+    }
+}
+
+
---
+++ CodeEval_CSharp_ValidParentheses/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+//using System.Threading;  // TODO remove
+
+namespace ValidParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText(args[0]))
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (null == line)
+                    continue;
+
+                    //Console.Out.WriteLine(line);
+                    Console.Out.WriteLine(isValid(line) ? "True" : "False");
+            }
+
+            //Thread.Sleep(10000);
+        }
+
+        static bool isValid(string line)
+        {
+            Stack<char> myStack = new Stack<char>();
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == '(' || line[i] == '{' || line[i] == '[')
+                {
+                    // push
+                    myStack.Push(line[i]);
+                }
+                else
+                {
+                    // try pop
+                    if (myStack.Count <= 0)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        char popped = myStack.Pop();
+                        //Console.Out.WriteLine("pop " + popped + " line[i] " + line[i]);
+                        if (popped != line[i] - 2 && popped != line[i] - 1)
+                        {
+                            // the specified characters must be within 
+                            // two of each other in the ascii table
+                            return false;
+                        }
+                    }
+                }
+            }
+            return myStack.Count == 0 ? true : false;
+        }
+    }
+}
+
